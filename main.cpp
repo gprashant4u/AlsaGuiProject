27087921343d9b594fc73e0520d0acb110f5328d
Prashant Kumar Gupta <gupta.prashant2000@gmail.com>
2026-04-25 06:38:56 +0000

    Finalized stable ALSA streaming with state preparation and pulse bridge

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <QDial>
 #include <QLabel>
 #include <QPushButton>
+#include <QTimer>
 #include "AlsaManager.h"
 
 int main(int argc, char *argv[]) {
@@ -16,23 +17,45 @@ int main(int argc, char *argv[]) {
     AlsaManager alsa;
     alsa.initAudio();
 
-    // Volume Control
+    // Volume UI
     layout->addWidget(new QLabel("Master Volume:"));
     QSlider *volSlider = new QSlider(Qt::Horizontal);
     volSlider->setRange(0, 100);
     volSlider->setValue(50);
     layout->addWidget(volSlider);
 
-    // Frequency Control
+    // Frequency UI
     layout->addWidget(new QLabel("Frequency (Hz):"));
     QDial *freqDial = new QDial();
     freqDial->setRange(200, 2000);
+    freqDial->setValue(440); // Standard 'A' note
     layout->addWidget(freqDial);
 
+    // Toggle Button
     QPushButton *btn = new QPushButton("Start Audio Stream");
+    btn->setCheckable(true);
     layout->addWidget(btn);
 
-    // Signals
+    // Continuous Audio Timer
+    QTimer *audioTimer = new QTimer();
+    
+    // This is the "Heartbeat" - it plays sound repeatedly while the button is ON
+    QObject::connect(audioTimer, &QTimer::timeout, [&]() {
+        alsa.playTone(freqDial->value(), 50); // Play a tiny 50ms slice
+    });
+
+    // Toggle Logic
+    QObject::connect(btn, &QPushButton::toggled, [&](bool checked) {
+        if (checked) {
+            audioTimer->start(45); // Trigger every 45ms (slight overlap for smoothness)
+            btn->setText("Stop Audio Stream");
+        } else {
+            audioTimer->stop();
+            btn->setText("Start Audio Stream");
+        }
+    });
+
+    // Volume Signal
     QObject::connect(volSlider, &QSlider::valueChanged, [&](int val) {
         alsa.setVolume(val);
     });
